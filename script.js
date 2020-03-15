9275779d9114cf2ca3047719efde687500f6d424
Terry <t3rrygong@gmail.com>
2020-03-14 18:39:38 -0600

    added shuffle array function and a function just to push random characters to the password array

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
+//an empty array to store password
+var password = [];
+
 //object of all the characters for password
 var passwordCharacters = {
   uppercase: ["A", "B", "C", "D", "E", "F", "G", "h", "I", "J", "k", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"],
@@ -18,47 +21,100 @@ function writePassword() {
 
 //password generator
 function generatePassword() {
-  //to confirm which type of characters you need and how long is the password
-  var confirmUppercase = confirm("Do you want uppercase characters? Click 'OK' for yes, click 'Cancel' for no.");
-  var confirmLowercase = confirm("Do you want lowercase characters? Click 'OK' for yes, click 'Cancel' for no.");
-  var confirmNumber = confirm("Do you want numbers? Click 'OK' for yes, click 'Cancel' for no.");
-  var confirmSpecialChar = confirm("Do you want special characters? Click 'OK' for yes, click 'Cancel' for no.");
-  var passwordLength = Number(prompt("Length of the password? Enter a number between 8~128."));
-  console.log(passwordLength);
+
+  //to confirm which type of characters user needs and how long is the password
+  var confirmUppercase = confirm("Do you want uppercase characters?\nClick 'OK' for yes, click 'Cancel' for no.");
+  var confirmLowercase = confirm("Do you want lowercase characters?\nClick 'OK' for yes, click 'Cancel' for no.");
+  var confirmNumber = confirm("Do you want numbers?\nClick 'OK' for yes, click 'Cancel' for no.");
+  var confirmSpecialChar = confirm("Do you want special characters?\nClick 'OK' for yes, click 'Cancel' for no.");
+  var passwordLength = Number(prompt("Length of the password?\nEnter a number between 8~128."));
+
+  //store how many of each characters we need
+  var numberOfUppercase, numberOfLowercase, numberOfnumeric, numberOfSpecialChar;
 
   //must enter a number between 8~128
   while (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    var passwordLength = Number(prompt("Length of the password? Enter a number between 8~128."));
-    console.log(passwordLength);
-    console.log(isNaN(passwordLength));
+    var passwordLength = Number(prompt("Invild entry.\nEnter a number between 8~128."));
   }
 
-  //an empty array to store password
-  var password = [];
-
-  //randomly generator password
-  for (let i = 0; i < passwordLength; i++) {
-    if (confirmUppercase === true && confirmLowercase === false && confirmNumber === false && confirmSpecialChar === false) {
-      password.push(randomPickerUppercase());
-    } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === false && confirmSpecialChar === false) {
-      password.push(randomPickerLowercase());
-    } else if (confirmUppercase === false && confirmLowercase === false && confirmNumber === true && confirmSpecialChar === false) {
-      password.push(randomPickerNumber());
-    } else if (confirmUppercase === false && confirmLowercase === false && confirmNumber === false && confirmSpecialChar === true) {
-      password.push(randomPickerSpecialChar());
-    } else if (confirmUppercase === true && confirmLowercase === false && confirmNumber === true) {
-
-    } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === true) {
-
-    } else if (confirmUppercase === true && confirmLowercase === true && confirmNumber === false) {
-
-    } else {
-
+  //check which type of characters user needs and tells getPassword how many of each characters we need
+  if (confirmUppercase === true && confirmLowercase === false && confirmNumber === false && confirmSpecialChar === false) {
+    getPassword(passwordLength);
+  } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === false && confirmSpecialChar === false) {
+    getPassword(null, passwordLength);
+  } else if (confirmUppercase === false && confirmLowercase === false && confirmNumber === true && confirmSpecialChar === false) {
+    getPassword(null, null, passwordLength);
+  } else if (confirmUppercase === false && confirmLowercase === false && confirmNumber === false && confirmSpecialChar === true) {
+    getPassword(null, null, null, passwordLength);
+  } else if (confirmUppercase === true && confirmLowercase === true && confirmNumber === false && confirmSpecialChar === false) {
+    numberOfUppercase = Math.floor(Math.random() * passwordLength) + 1;
+    while (numberOfUppercase === 0 || numberOfUppercase === passwordLength) {
+      numberOfUppercase = Math.floor(Math.random() * passwordLength) + 1;
     }
+    numberOfLowercase = passwordLength - numberOfUppercase;
+    console.log(numberOfUppercase);
+    console.log(numberOfLowercase);
+    getPassword(numberOfUppercase, numberOfLowercase);
+  } else if (confirmUppercase === true && confirmLowercase === false && confirmNumber === true && confirmSpecialChar === false) {
+
+  } else if (confirmUppercase === true && confirmLowercase === false && confirmNumber === false && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === true && confirmSpecialChar === false) {
+
+  } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === false && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === false && confirmLowercase === false && confirmNumber === true && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === false && confirmLowercase === true && confirmNumber === true && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === true && confirmLowercase === false && confirmNumber === true && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === true && confirmLowercase === true && confirmNumber === false && confirmSpecialChar === true) {
+
+  } else if (confirmUppercase === true && confirmLowercase === true && confirmNumber === true && confirmSpecialChar === false) {
+
   }
+
+  //shuffle password
+  password = shuffle(password);
 
   //return password in string
   return password.join("");
+}
+
+//randomly add characters to password array
+function getPassword(numberOfUppercase, numberOfLowercase, numberOfnumeric, numberOfSpecialChar) {
+  if (numberOfUppercase > 0) {
+    for (i = 0; i < numberOfUppercase; i++) {
+      password.push(randomPickerUppercase());
+    }
+  }
+  if (numberOfLowercase > 0) {
+    for (i = 0; i < numberOfLowercase; i++) {
+      password.push(randomPickerLowercase());
+    }
+  }
+  if (numberOfnumeric > 0) {
+    for (i = 0; i < numberOfnumeric; i++) {
+      password.push(randomPickerNumber());
+    }
+  }
+  if (numberOfSpecialChar > 0) {
+    for (i = 0; i < numberOfSpecialChar; i++) {
+      password.push(randomPickerSpecialChar());
+    }
+  }
+}
+
+//shuffle array
+function shuffle(arr) {
+  var copy = [];
+  for (i = 0; i < arr.length; i++) {
+    var current = Math.floor(Math.random() * arr.length) + 1;
+    copy.push(arr[current]);
+    arr.splice(current, 1);
+  }
+  return copy;
 }
 
 //random pick character from password object
